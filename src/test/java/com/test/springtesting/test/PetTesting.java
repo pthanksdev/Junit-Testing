1984bd6e12f1d60cb4a104d25962d5682d447c09
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-01-30 01:27:59 +0100

    pet testing2

--- src/test/java/com/test/springtesting/test/PetTesting.java
+++ src/test/java/com/test/springtesting/test/PetTesting.java
@@ -1,25 +1,28 @@
 package com.test.springtesting.test;
 
 import com.test.springtesting.model.entity.Pet;
-
 import com.test.springtesting.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;  // Changed from BeforeAll
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(SpringExtension.class)
 public class PetTesting {
-    @Autowired
-    static PetRepository petRepository;
 
-    @BeforeAll
-    static void setUp() {
+    @Autowired
+    PetRepository petRepository;  // Removed 'static'
+
+    @BeforeEach  // Changed from @BeforeAll
+    void setUp() {  // Removed 'static'
         Pet pet1 = new Pet();
         pet1.setAge(3);
         pet1.setName("pet1");
@@ -51,14 +54,25 @@ public class PetTesting {
     void testSave_Success() {
         Pet newPet = new Pet();
         newPet.setAge(3);
-        newPet.setName("pet4");
+        newPet.setName("pet5");  // Consider changing to pet5 to avoid duplicate
         newPet.setBreed("Dog");
         newPet.setGender("F");
-
 
         Pet savedPet = petRepository.save(newPet);
 
         Assertions.assertNotNull(savedPet);
         Assertions.assertEquals(newPet.getName(), savedPet.getName());
+
+        System.out.println();
+    }
+
+    @Test
+    void testSearchByBreed_Success() {
+        String breed = "Cat";
+        int expected = 3;
+
+        List<Pet> search = petRepository.findByBreed(breed);
+
+        Assertions.assertEquals(expected, search.size());
     }
 }
