256398bfcc66a078d75d0935ce87780d4541d056
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-01-31 14:35:19 +0100

    fix bug

--- src/main/java/com/test/springtesting/repository/PetRepository.java
+++ src/main/java/com/test/springtesting/repository/PetRepository.java
@@ -2,7 +2,12 @@ package com.test.springtesting.repository;
 
 
 import com.test.springtesting.model.entity.Pet;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepository extends JpaRepository<Pet, Integer> {
+
+    List<Pet> findByBreed(String breed);
 }
